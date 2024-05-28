1413e3cf05d1a74d353cbaa736e5bac20fa032a2
ArthurLorphelin <arthur.lorphelin@yahoo.fr>
2024-05-28 15:32:44 +0200

    Change username to email in .xml file Change Java class from MainActivity to SignIn Change .xml file from activity_main to sign_in

--- app/src/main/java/com/example/isepdevappmobileadmin/SignIn.java
+++ app/src/main/java/com/example/isepdevappmobileadmin/SignIn.java
@@ -5,13 +5,13 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SignIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sign_in);
 
     }
 }
