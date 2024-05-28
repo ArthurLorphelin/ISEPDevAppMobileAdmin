ca1f9eb08e0fc4f1fac7270914e2bf7a1cf23dbf
ArthurLorphelin <arthur.lorphelin@yahoo.fr>
2024-05-28 16:43:56 +0200

    Creation of the Adapter for the spinner list

--- app/src/main/java/com/example/isepdevappmobileadmin/SignUp.java
+++ app/src/main/java/com/example/isepdevappmobileadmin/SignUp.java
@@ -1,6 +1,8 @@
 package com.example.isepdevappmobileadmin;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +14,10 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.sign_up);
+
+        Spinner spinnerAdminRoles = findViewById(R.id.admin_roles_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.admin_role, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerAdminRoles.setAdapter(adapter);
     }
 }
