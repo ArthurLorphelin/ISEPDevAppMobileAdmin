80875c19d9a4860ff6f8806e86fb4ad5e9145c10
ArthurLorphelin <arthur.lorphelin@yahoo.fr>
2024-05-28 17:18:28 +0200

    import classes Spinner and ArrayAdapter

--- app/src/main/java/com/example/isepdevappmobileadmin/SignUp.java
+++ app/src/main/java/com/example/isepdevappmobileadmin/SignUp.java
@@ -3,7 +3,9 @@ package com.example.isepdevappmobileadmin;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
